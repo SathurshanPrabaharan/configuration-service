1fd4b774e7bc6717faf90d8e0c399fa6a7494871
sathurshanath <sathurshanath2021@gmail.com>
2024-05-15 18:13:36 +0530

    Implement Department CRUD APIs

---
+++ src/main/java/com/configurationservice/DTO/Response/Admin/DepartmentResponseMessage.java
@@ -0,0 +1,15 @@
+package com.configurationservice.DTO.Response.Admin;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DepartmentResponseMessage {
+
+    private String message;
+
+}
